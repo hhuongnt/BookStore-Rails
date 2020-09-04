b19c5e0173d66cbbf7e022c1fa59ef525134b21f
mabel <mabel.nguyen.goldenowl@gmail.com>
2020-09-05 02:13:25 +0700

    fix code

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'hhuongnt98@gmail.com'
+  config.mailer_sender = 'n16dcat026@student.ptithcm.edu.vn'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
